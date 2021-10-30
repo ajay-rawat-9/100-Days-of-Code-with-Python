33524c6be0e46c5d22cefec209c64a57640e36b7
ajay-rawat-9 <84247810+ajay-rawat-9@users.noreply.github.com>
2021-10-30 19:32:29 +0530

    Updated

--- Trailing_Zero.py
+++ Trailing_Zero.py
@@ -1,1 +1,10 @@
-
+class Solution:
+  def Trailing_Zero(self,N):
+    temp=N
+    trail=0
+    while temp:
+       trail=temp//5
+       temp//=5
+    return trail
+N=int(input())
+print("The number of trailing zeros in the factorial of",N,"are:",Solution().Trailing_Zero(N))
